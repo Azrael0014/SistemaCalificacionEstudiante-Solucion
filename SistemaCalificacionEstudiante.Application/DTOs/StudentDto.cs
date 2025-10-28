21674507de3b6f622fc092a1f1ecd4fca91dd501
Randy Radhames Batista Perez <r.batista37@unapec.edu.do>
2025-10-27 23:53:40 -0400

    Avances 8

---
+++ SistemaCalificacionEstudiante.Application/DTOs/StudentDto.cs
@@ -0,0 +1,13 @@
+﻿namespace SistemaCalificacionEstudiante.Application.DTOs
+{
+    public class StudentDto
+    {
+        public int Id { get; set; }
+        public string Matricula { get; set; }
+        public string FullName { get; set; }
+        public int Age { get; set; }
+        public string Carrera { get; set; }
+        public string Email { get; set; }
+        public DateTime FechaInscripcion { get; set; } = DateTime.Today;
+    }
+}
