ddb314b31bd02167e6cfc92ed6cf05d3208dcbe1
Randy Radhames Batista Perez <r.batista37@unapec.edu.do>
2025-10-28 21:09:15 -0400

    Avances8

--- SistemaCalificacionEstudiante.Api/Controllers/StudentsController.cs
+++ SistemaCalificacionEstudiante.Api/Controllers/StudentsController.cs
@@ -1,1 +1,81 @@
-﻿
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SistemaCalificacionEstudiante.Application.DTOs;
+using SistemaCalificacionEstudiante.Application.Interfaces;
+
+namespace SistemaCalificacionEstudiante.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize] 
+    public class StudentsController : ControllerBase
+    {
+        private readonly IStudentService _studentService;
+
+        public StudentsController(IStudentService studentService)
+        {
+            _studentService = studentService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllStudents()
+        {
+            var students = await _studentService.GetAllStudentsAsync();
+            return Ok(students);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetStudentById(int id)
+        {
+            var student = await _studentService.GetStudentByIdAsync(id);
+            if (student == null)
+            {
+                return NotFound(new { Message = "Estudiante no encontrado." });
+            }
+            return Ok(student);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddStudent([FromBody] StudentDto studentDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            await _studentService.AddStudentAsync(studentDto);
+
+            return Ok(studentDto);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateStudent(int id, [FromBody] StudentDto studentDto)
+        {
+            if (id != studentDto.Id)
+            {
+                return BadRequest(new { Message = "El ID del estudiante no coincide." });
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            await _studentService.UpdateStudentAsync(id, studentDto);
+
+            
+
+            return Ok(new { Message = "Estudiante actualizado exitosamente." });
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteStudent(int id)
+        {
+            await _studentService.DeleteStudentAsync(id);
+
+            
+
+            return Ok(new { Message = "Estudiante eliminado exitosamente." }); 
+        }
+    }
+}
