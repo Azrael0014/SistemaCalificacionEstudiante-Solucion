608fb5d0222b14ad2371e6c84b622ab487a49872
Randy Radhames Batista Perez <r.batista37@unapec.edu.do>
2025-11-05 21:25:18 -0400

    Avances10

--- SistemaCalificacionEstudiante.Api/Program.cs
+++ SistemaCalificacionEstudiante.Api/Program.cs
@@ -1,52 +1,40 @@
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
+using SistemaCalificacionEstudiante.Application;
+using SistemaCalificacionEstudiante.Infrastructure;
 using Microsoft.OpenApi.Models;
-using SistemaCalificacionEstudiante.Application.Interfaces;
-using SistemaCalificacionEstudiante.Application.Services;
-using SistemaCalificacionEstudiante.Infrastructure.Context;
-using SistemaCalificacionEstudiante.Infrastructure.Repositories;
-using SistemaCalificacionEstudiante.Infrastructure.Services; 
-using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
 builder.Services.AddControllers();
-
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-builder.Services.AddScoped<IUserRepository, UserRepository>();
-builder.Services.AddScoped<IStudentRepository, StudentRepository>();
-builder.Services.AddScoped<IMateriaRepository, MateriaRepository>();
-builder.Services.AddScoped<ICalificacionRepository, CalificacionRepository>();
-
-builder.Services.AddScoped<IAuthService, AuthService>();
-builder.Services.AddScoped<IStudentService, StudentService>();
-builder.Services.AddScoped<IMateriaService, MateriaService>();
-builder.Services.AddScoped<ICalificacionService, CalificacionService>();
-
-builder.Services.AddScoped<IJwtTokenGenerator, JwtTokenGenerator>();
-
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(options =>
-    {
-        options.TokenValidationParameters = new TokenValidationParameters
-        {
-            ValidateIssuer = true,
-            ValidateAudience = true,
-            ValidateLifetime = true,
-            ValidateIssuerSigningKey = true,
-            ValidIssuer = builder.Configuration["JwtSettings:Issuer"],
-            ValidAudience = builder.Configuration["JwtSettings:Audience"],
-            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JwtSettings:Key"]!))
-        };
-    });
-
+builder.Services.AddApplicationServices();
+builder.Services.AddInfrastructureServices(builder.Configuration);
 builder.Services.AddEndpointsApiExplorer();
+
 builder.Services.AddSwaggerGen(options =>
 {
+    options.TagActionsBy(api =>
+    {
+        var controllerName = api.ActionDescriptor.RouteValues["controller"];
+
+        if (controllerName == null)
+        {
+            return new[] { "Default" };
+        }
+
+        switch (controllerName)
+        {
+            case "Auth":
+                return new[] { "1. Auth" };
+            case "Students":
+                return new[] { "2. Students" };
+            case "Materias":
+                return new[] { "3. Materias" };
+            case "Calificaciones":
+                return new[] { "4. Calificaciones" };
+            default:
+                return new[] { $"5. {controllerName}" };
+        }
+    });
+
     options.SwaggerDoc("v1", new OpenApiInfo { Title = "Sistema Calificaciones API", Version = "v1" });
 
     options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
@@ -85,8 +73,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 
-app.UseAuthentication(); 
-app.UseAuthorization();  
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapControllers();
 
